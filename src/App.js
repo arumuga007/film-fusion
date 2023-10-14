0ee83fe8291b38d51a1a0638cf76bdd22a88f777
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-10-14 12:49:32 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import UseEffectExample from './components/UseEffectExample';
 function App() {
   return (
     <div className="App">
-      <UseEffectExample />
       <h1>Hello World</h1>
     </div>
   );
