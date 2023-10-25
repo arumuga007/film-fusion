4a9a6ca044b3eafed2f0d6e606c075e9144ccf82
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-10-25 12:47:53 +0530

    upcoming movies data fetched and displayed successfully

--- src/components/HomeBody/SubBody.jsx
+++ src/components/HomeBody/SubBody.jsx
@@ -3,7 +3,7 @@ import styles from './../../style/BodyStyles/SubBody.module.css';
 import UpcomingMovies from './SubBody/UpcomingMovies';
 import UpcomingMoviesSkeleton from './SubBody/UpcomingMoviesSkeleton';
 
-const getUpcomingMoviesSkeleton = () => {
+export const getUpcomingMoviesSkeleton = () => {
     let itemsToReturn = [];
     for(let i = 0; i < 4; i++) {
         itemsToReturn.push(<UpcomingMoviesSkeleton />);
@@ -12,13 +12,37 @@ const getUpcomingMoviesSkeleton = () => {
 }
 
 const SubBody = () => {
+    let timeOut = 0;
     const [upcomingMovie, setUpcomingMovie] = useState(false);
+    const url = 'https://imdb8.p.rapidapi.com/title/get-coming-soon-movies?homeCountry=US&purchaseCountry=US&currentCountry=US';
+    const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1f56e93584mshececd60fb33af54p119ec4jsn3077292436e3',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+    useEffect(() => {
+        fetch(url, options)
+        .then(response => response.json())
+        .then((data) => {
+            setUpcomingMovie(data.slice(0,5));
+        })
+        .catch((error) => {
+            console.log('error occured during fetching a title Id  for upcoming movies', error);
+        })
+    }, []);
+
     return (
         <div className={styles['sub-body']}>
             <div className={styles.upcomingMoviesHeader}>Upcoming Movies</div>
             {
                 upcomingMovie
-                ? <UpcomingMovies />
+                ? upcomingMovie.map((movieTitle) => {
+                    timeOut += 500;
+                    let title = movieTitle.id.split('/')[2];
+                    return <UpcomingMovies title={title} releaseDate={movieTitle.releaseDate} timeOut={timeOut} />
+                })
                 : getUpcomingMoviesSkeleton()
             }
         </div>
--- src/components/HomeBody/SubBody/UpcomingMovies.jsx
+++ src/components/HomeBody/SubBody/UpcomingMovies.jsx
@@ -1,12 +1,62 @@
 import styles from './../../../style/BodyStyles/SubBody.module.css';
+import { getUpcomingMoviesSkeleton } from '../SubBody';
+import { useEffect, useState } from 'react';
+const UpcomingMovies = (props) => {
+    const [upcomingMovie, setUpcomingMovie] = useState(false);
+    const [render, setRender] = useState(0);
+    const url = `https://imdb8.p.rapidapi.com/title/get-details?tconst=${props.title}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1f56e93584mshececd60fb33af54p119ec4jsn3077292436e3',
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+    useEffect(() => {
+        // fetch(url, options)
+        // .then(response => response.json())
+        // .then((data) => {
+        //     setUpcomingMovie(data);
+        // })
+        // .catch((error) => {
+        //     console.log('error occured while getting a upcoming movies', error);
+        // })
+        setTimeout(() => {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch(url, options);
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    const data = await response.json();
+                    setUpcomingMovie(data);
+                } catch (error) {
+                    setRender(render + 1);
+                    console.error('Error occurred while getting upcoming movie details', error);
+                }
+            };
+        
+            fetchData();
+        }, props.timeOut)
 
-const UpcomingMovies = () => {
+    }, [render])
 
     return(
-        <div>
-            
-        </div>
+        <>
+        {upcomingMovie 
+        ?
+            <div className={styles.UpcomingMovieContainer}>
+                <div className={styles['upcoming-movie-image-container']}>
+                <img src={upcomingMovie.image.url} className={styles['upcoming-movie-image']} />
+                </div>
+                 <div className={styles['upcoming-movie-title']}>{upcomingMovie.title}</div>
+                <div className={styles['release-date']}>release date: {props.releaseDate}</div>
+            </div>
+
+        : getUpcomingMoviesSkeleton()}
+        </>
     )
+
 }
 
 export default UpcomingMovies;
