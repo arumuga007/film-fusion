14bbc05c6d6c8f573331d06bd85f53aaee382ca3
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-11-03 09:48:09 +0530

    Issue related to zoom level and logo fixed

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import styles from './../../style/NavBarStyle.module.css';
 const Logo = () => {
     return(
         <div className={styles['logo-container']}>
-            <img src={process.env.PUBLIC_URL + '/images/Logo.png'} className={styles['logo']}></img>
+            <img src={process.env.PUBLIC_URL + '/images/logo.png'} className={styles['logo']}></img>
         </div>
     )
 }
