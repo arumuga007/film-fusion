b4c0c52bfdded678a1383aab3dc34fc3e7dc3cca
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-11-06 11:42:28 +0530

    category page optimised for mobile view

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -5,60 +5,60 @@ const Categories = () => {
     return (
         <div className={styles['categories-container']}>
             <Link to='/film-fusion/action-movies' className={styles['category-item']}>
-                <img src={process.env.PUBLIC_URL + '/images/Category/comedyMovie.jpg'} />
+                <img src={process.env.PUBLIC_URL + '/images/Category/actionMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
+                    <div className={styles['title']}>Action</div>
                     <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
+            <Link to='/film-fusion/drama-movies' className={styles['category-item']}>
                 <img src={process.env.PUBLIC_URL + '/images/Category/History.jpg'} />
                 <div className={styles['image-cover']}>
                     <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['description']}>Compelling Human Stories</div>
                 </div>
             </Link>
 
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
+            <Link to='/film-fusion/romantic-movies' className={styles['category-item']}>
                 <img src={process.env.PUBLIC_URL + '/images/Category/romanticMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Romantic</div>
+                    <div className={styles['description']}>Heartwarming Love Stories</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
+            <Link to='/film-fusion/thriller-movies' className={styles['category-item']}>
                 <img src={process.env.PUBLIC_URL + '/images/Category/Thriller.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Thriller</div>
+                    <div className={styles['description']}>Heart-Pounding Thriller Adventures</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
+            <Link to='/film-fusion/comedy-movies' className={styles['category-item']}>
                 <img src={process.env.PUBLIC_URL + '/images/Category/comedyMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Comedy</div>
+                    <div className={styles['description']}>Feel-Good Comedic Entertainment</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
-                <img src={process.env.PUBLIC_URL + '/images/Category/comedyMovie.jpg'} />
+            <Link to='/film-fusion/science-movies' className={styles['category-item']}>
+                <img src={process.env.PUBLIC_URL + '/images/Category/scienceMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Science</div>
+                    <div className={styles['description']}>Mind-Bending Sci-Fi Adventures</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
-                <img src={process.env.PUBLIC_URL + '/images/Category/comedyMovie.jpg'} />
+            <Link to='/film-fusion/crime-movies' className={styles['category-item']}>
+                <img src={process.env.PUBLIC_URL + '/images/Category/crimeMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Crime</div>
+                    <div className={styles['description']}>Dark and Compelling Stories</div>
                 </div>
             </Link>
-            <Link to='/film-fusion/action-movies' className={styles['category-item']}>
-                <img src={process.env.PUBLIC_URL + '/images/Category/comedyMovie.jpg'} />
+            <Link to='/film-fusion/horror-movies' className={styles['category-item']}>
+                <img src={process.env.PUBLIC_URL + '/images/Category/horrorMovie.jpg'} />
                 <div className={styles['image-cover']}>
-                    <div className={styles['title']}>Drama</div>
-                    <div className={styles['description']}>Action-packed Blockbuster Entertainment</div>
+                    <div className={styles['title']}>Horror</div>
+                    <div className={styles['description']}>Spine-Tingling Terror Adventures</div>
                 </div>
             </Link>
         </div>
