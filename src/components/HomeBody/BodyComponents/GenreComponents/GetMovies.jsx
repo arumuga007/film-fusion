e24ece462c93e338af7665e5ceb2ab4ef67d7d43
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-11-06 15:02:46 +0530

    Upcoming features page UI optimised for mobile view

--- src/components/HomeBody/BodyComponents/GenreComponents/GetMovies.jsx
+++ src/components/HomeBody/BodyComponents/GenreComponents/GetMovies.jsx
@@ -11,7 +11,7 @@ export const getSkeleton = () => {
 }
 
 const GetMovies = (props) => {
-    const url = `https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre?genre=${props.category}&limit=4`;
+    const url = `https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre?genre=${props.category}&limit=2`;
     const options = {
         method: 'GET',
         headers: {
--- src/components/UpcomingFeatures/UpcomingFeatures.jsx
+++ src/components/UpcomingFeatures/UpcomingFeatures.jsx
@@ -11,7 +11,9 @@ const UpcomingFeatures = () => {
                     We are working on it
                 </div>
                 <div className={styles.description}>
-                    Great news! We're hard at work on this feature, and it's coming your way soon.<br /> Thanks for your patience and stay tuned for something awesome! 😊🚀
+                    <p>
+                    Great news! We're hard at work on this feature, and it's coming your way soon.</p>
+                    <p> Thanks for your patience and stay tuned for something awesome! 😊🚀</p>
                 </div>
                 <a className={styles['contribute-btn']} href='mailto:arumugaperumal073@gmail.com'>
                     Wanna Contribute
